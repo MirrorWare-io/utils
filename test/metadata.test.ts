68df4fe8bfc60514c6d192cacb36765874cd4646
Benjythebee <Benjy.larcher@aol.com>
2023-12-19 09:09:28 +1300

    test

--- test/metadata.test.ts
+++ test/metadata.test.ts
@@ -1,4 +1,4 @@
 import {getCbContractsByChainId,getMetadataForNFTs} from '../index'
 // getMetadataForNFTs('0x97539a2a2125ce3e80885be35e2637d4280a5e8a', ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],5).then(console.log)
 
-getMetadataForNFTs('0x653271bb96969643Ba45F622D6113a6Cd8b06ae2', ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],5).then(console.log)
+// getMetadataForNFTs('0x653271bb96969643Ba45F622D6113a6Cd8b06ae2', ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],5).then(console.log)
